1de54fffa26f23c3b2f57cfbc10d70f31c3f4376
acj <gitlab@acjs.net>
2020-04-29 22:33:19 +0200

    Change Twitter profile image PoC

---
+++ twitter/avatar.php
@@ -0,0 +1,24 @@
+<?php
+
+error_reporting(-1);
+
+$config = parse_ini_file(dirname(__DIR__) . '/config.ini');
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+$settings = array(
+    'oauth_access_token' => $config['twitter_access_token'],
+    'oauth_access_token_secret' => $config['twitter_access_token_secret'],
+    'consumer_key' => $config['twitter_api_key'],
+    'consumer_secret' => $config['twitter_api_secret_key']
+);
+
+$url = 'https://api.twitter.com/1.1/account/update_profile_image.json';
+$file = file_get_contents('564011_10150987745223956_1777701739_n.jpg');
+$image = base64_encode($file);
+$postfield = ['image' => $image];
+$requestMethod = 'POST';
+$twitter = new TwitterAPIExchange($settings);
+echo $twitter->setPostfields($postfield)
+    ->buildOauth($url, $requestMethod)
+    ->performRequest();

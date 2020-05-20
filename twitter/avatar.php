333d499175793d644ee8d0d43e88e4fe5b84ee94
acj <gitlab@acjs.net>
2020-05-20 18:40:37 +0200

    Get deIcon

--- twitter/avatar.php
+++ twitter/avatar.php
@@ -8,7 +8,7 @@ $config = parse_ini_file(dirname(__DIR__) . '/config.ini');
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
-$settings = array(
+/*$settings = array(
     'oauth_access_token' => $config['twitter_access_token'],
     'oauth_access_token_secret' => $config['twitter_access_token_secret'],
     'consumer_key' => $config['twitter_api_key'],
@@ -16,11 +16,18 @@ $settings = array(
 );
 
 $url = 'https://api.twitter.com/1.1/account/update_profile_image.json';
-$file = file_get_contents('541355_10150864051868956_181292363_n.jpg');
+$file = file_get_contents('564011_10150987745223956_1777701739_n.jpg');
 $image = base64_encode($file);
 $postfield = ['image' => $image];
 $requestMethod = 'POST';
 $twitter = new TwitterAPIExchange($settings);
 echo $twitter->setPostfields($postfield)
     ->buildOauth($url, $requestMethod)
-    ->performRequest();
+    ->performRequest();*/
+
+
+require_once '../../heticoon/src/class.deicon.php';
+
+$logo = new Deicon;
+echo $logo;
+
